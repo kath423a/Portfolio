b5bc3292e8b8269385b1a53c285edb3b33288c4f
Kathrine <kath423a@stud.kea.dk>
2021-04-09 08:24:33 +0200

    cooltext-effect and and burgermenu working

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 "use script";
 
-window.addEventListener("DOMContentLoaded", sidenVises);
+window.addEventListener("DOMContentLoaded", start);
 
-function sidenVises() {
+// Global variables
+let splittext;
+
+function start() {
   console.log("Lets gooooo");
 
+  //Create a global variable and select the text from the html
+  let sentence = document.querySelector("#cooltext");
+
+  console.log(sentence);
+
+  //Split text into seperate letters/characters
+  splittext = sentence.textContent.split("");
+  console.log(splittext);
+
+  //Remove existing text
+  sentence.textContent = "";
+
+  //For each character in the text, create a span element and put the character inside the span element
+  splittext.forEach((letter, index) => {
+    const span = document.createElement("span");
+
+    span.classList.add("fulltext-animation");
+    span.style.setProperty("--letter", index);
+
+    if (letter === " ") {
+      span.innerHTML = "&nbsp";
+    } else {
+      span.textContent = letter;
+    }
+
+    sentence.append(span);
+  });
+
+  //Click burgermenu
   document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
 
