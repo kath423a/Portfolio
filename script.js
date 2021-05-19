62cf3404b3e2f6c41ded46b60bb31f3c6286b18c
Kathrine <kath423a@stud.kea.dk>
2021-05-19 10:25:52 +0200

    loading ikon working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use script";
 
-window.addEventListener("DOMContentLoaded", start);
+window.addEventListener("load", start);
 
 // Global variables
 let splittext;
@@ -8,7 +8,7 @@ var myVar;
 
 function start() {
   console.log("Lets gooooo");
-  // document.querySelector("#loading").classList.add(".hide");
+  document.querySelector("#loading").style.display = "none";
 
   //Create a global variable and select the text from the html
   let sentence = document.querySelector("#cooltext");
@@ -40,12 +40,6 @@ function start() {
 
   //Click burgermenu
   document.querySelector("#menuknap").addEventListener("click", toggleMenu);
-
-  myVar = setTimeout(timerSet, 2000);
-}
-
-function timerSet() {
-  document.getElementById("loading").style.display = "none";
 }
 
 const navButton = document.querySelector("button");
